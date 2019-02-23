2520def3925d459d99a77815177d586ec09e3420
byRamon <46496290+byRamon@users.noreply.github.com>
2019-02-23 12:09:46 -0600

    Agregar archivos de proyecto.

---
+++ Practica05Productos/Data/ProductosDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Practica05Productos.Models;
+
+namespace Practica05Productos.Data
+{
+    public class ProductosDbContext : DbContext
+    {
+        public ProductosDbContext(DbContextOptions<ProductosDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ Practica05Productos/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practica05Productos.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        [Range(0, 99, ErrorMessage = "Le existencia debe estar en el arango de 0 a 99")]
+        public int Existencia { get; set; }
+        [Display(Name ="Producto descontinuado")]
+        public bool Descontinuado { get; set; }
+    }
+}
